da92ad20f3ac48017bf44eb10f8a12daccc9172a
Pete Hawkins <pete@phawk.co.uk>
2017-06-12 13:53:56 +0100

    Initial integration of da-core

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
 
+gem "da-core", path: "/Users/pete/Sites/services/da-core"
+
 # Specify your gem's dependencies in user-auth.gemspec
 gemspec
--- config/boot.rb
+++ config/boot.rb
@@ -5,21 +5,9 @@ $: << File.expand_path("../../lib", __FILE__)
 # Default ENV to dev if not present
 ENV["APP_ENV"] ||= "development"
 
-# Autoload common standard library features
-require "json"
-
-# Autoload gems from the Gemfile
-require "bundler/setup"
-require "dotenv"
-
-require "active_support/core_ext/object/blank" # blank? and present?
-require "active_support/core_ext/integer/time" # 1.week.ago etc
-require "active_support/core_ext/hash/slice" # params.slice(:one, :other)
-require "active_support/core_ext/hash/conversions" # params.symbolize_keys
-require "active_support/core_ext/hash/indifferent_access"
-
-# Load dev env vars
-Dotenv.load if %w[development test].include? ENV["APP_ENV"]
+require "da/core/environment"
+require "da/db"
+require "da/web"
 
 # Autoload app dependencies
 path = File.join(__dir__, "initializers", "*.rb")
